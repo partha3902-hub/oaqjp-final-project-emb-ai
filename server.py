a95e6eddc0eb40997b2260ed925eb00eee7c7126
partha3902-hub <partha3902@gmail.com>
2025-10-06 04:35:26 -0400

    errorhandlingcommit

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
+"""
+This server interface is web application deployment for
+emotion detection application
+Calls emotion detection analyser function
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    This function gets an input from a text box 
+    calls an emotion detector function to find the emotion scores
+    of the text and finds the dominant emotion. In case of invalid input
+    it displays an error
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -19,11 +30,16 @@ def sent_analyzer():
     dominant = response['dominant_emotion']
     if dominant is None:
         return "Invalid text! Please try again!."
-    else:
     # Return a formatted string with the emotion label and score
-        return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant)
+    message = f"For the given statement, the system response is 'anger': {anger_score}, \
+    'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. \
+    The dominant emotion is {dominant}."
+    return message
 @app.route("/")
 def render_index_page():
+    """
+    This is a rendering function for the emotion detection web application
+    """
     return render_template('index.html')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
